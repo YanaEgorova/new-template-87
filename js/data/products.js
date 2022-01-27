fd589d2b4fbec689d92a5bdb231784da73b7b8bf
Yana <yanaegorova69.3@gmail.com>
2022-01-27 02:11:37 +0200

    +

---
+++ js/data/products.js
@@ -0,0 +1,366 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Sticker',
+        price: 0.99,
+        image: 'https://m.media-amazon.com/images/I/81EGjksf6IS._SL1500_.jpg',
+        description: [
+            {
+                text: 'Motivational Sticker to get you throughout the day!',
+            },
+            {
+                text: '3 x 3 inches',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-2',
+        name: 'Product Title',
+        price: 5.95,
+        image: 'https://m.media-amazon.com/images/I/61WgmLgwuQL._AC_SL1023_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-3',
+        name: 'Product Title',
+        price: 19.99,
+        image: 'https://m.media-amazon.com/images/I/71NMrhZel6L._SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-4',
+        name: 'Product Title',
+        price: 24.99,
+        image: 'https://m.media-amazon.com/images/I/71K2-Ib6wIL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+           
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-5',
+        name: 'Product Title',
+        price: 39.99,
+        image: 'https://m.media-amazon.com/images/I/71IywmQMCTL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-6',
+        name: 'Product Title',
+        price: 49.95,
+        image: 'https://m.media-amazon.com/images/I/81C1DGnCNsL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-7',
+        name: 'Product Title',
+        price: 89.99,
+        image: 'https://m.media-amazon.com/images/I/61kwRNPtMpL._SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-8',
+        name: 'Product Title',
+        price: 106.58,
+        image: 'https://m.media-amazon.com/images/I/61O9tWR6WDS._SL1475_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-9',
+        name: 'Product Title',
+        price: 109.00,
+        image: 'https://m.media-amazon.com/images/I/812wiz16nlL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-10',
+        name: 'Product Title',
+        price: 115.89,
+        image: 'https://m.media-amazon.com/images/I/61hqjITqd5L._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-11',
+        name: 'Product Title',
+        price: 119.00,
+        image: 'https://m.media-amazon.com/images/I/61Q+4GtGuAL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+          
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-12',
+        name: 'Product Title',
+        price: 122.00,
+        image: 'https://m.media-amazon.com/images/I/81E4iCssGEL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-13',
+        name: 'Product Title',
+        price: 124.53,
+        image: 'https://m.media-amazon.com/images/I/91EpbuMiGuL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+         
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+    {
+        id: 'product-14',
+        name: 'Product Title',
+        price: 129.87,
+        image: 'https://m.media-amazon.com/images/I/91C-AH4m-EL._AC_SL1500_.jpg',
+        description: [
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+            {
+                text: '',
+            },
+        ],
+        images: [],
+        isItRing: false,
+        ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
+    },
+]
