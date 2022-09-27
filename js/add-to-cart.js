9364c9e5f03a6486b91fa2e6c76f221b7bf61825
Yana <yanaegorova69.3@gmail.com>
2022-09-28 01:49:14 +0300

    +

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -52,11 +52,14 @@ function addToCart(e) {
        }
     })
 
-    // ADD TO LOCAL STORAGE
-    if (okay){
-        cartSpan.textContent = Number(cartSpan.textContent) + 1;
+     // ADD TO LOCAL STORAGE
+     if (okay){
+        if(localStorage(productId) != false) {
+            if(cartSpan) {
+                cartSpan.textContent = Number(cartSpan.textContent) + 1;
+            }
+        }
         showSuccessMessage(successMessage, successMessageSpan, name);
-        localStorage(productId);
     }
     
 
